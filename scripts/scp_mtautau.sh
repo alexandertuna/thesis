dc27085f19a0c2539f91687296b870657edf89c0
alexandertuna <alexander.tuna@gmail.com>
2014-11-25 11:35:26 +0100

    update with truth met

--- scripts/scp_mtautau.sh
+++ scripts/scp_mtautau.sh
@@ -1,7 +1,7 @@
 # a script to retrieve plots from at3i00. run from the top dir.
 # [thesis] > . scripts/scp_mtautau.sh
 
-input="atuna@at3i00.hep.upenn.edu:/web/Analysis/tuna/HiggsTauTau2012/scratch/mtautau-2014-11-25-04h44m49s"
+input="atuna@at3i00.hep.upenn.edu:/web/Analysis/tuna/HiggsTauTau2012/scratch/mtautau-2014-11-25-05h34m14s"
 output="figures/mtautau"
 
 mkdir -p ${output}
