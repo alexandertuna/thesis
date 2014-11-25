ceb5c16aad30f01e9fb8df17bde1ae35c277c24d
alexandertuna <alexander.tuna@gmail.com>
2014-11-25 15:44:08 +0100

    include boost

--- scripts/scp_mtautau.sh
+++ scripts/scp_mtautau.sh
@@ -1,12 +1,12 @@
 # a script to retrieve plots from at3i00. run from the top dir.
 # [thesis] > . scripts/scp_mtautau.sh
 
-input="atuna@at3i00.hep.upenn.edu:/web/Analysis/tuna/HiggsTauTau2012/scratch/mtautau-2014-11-25-05h34m14s"
+input="atuna@at3i00.hep.upenn.edu:/web/Analysis/tuna/HiggsTauTau2012/scratch/mtautau-2014-11-25-09h21m29s"
 output="figures/mtautau"
 
 mkdir -p ${output}
 
-for plot in "mtautau-roc"; do 
+for plot in "mtautau-ROC-vbf" "mtautau-ROC-boost"; do
     for ext in "eps" "pdf"; do
         scp ${input}/${plot}.${ext} ${output}/${plot}.${ext}
     done
