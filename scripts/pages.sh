9bea41c7e8bd813c4ddcf4ab25caadd57384aeba
alexandertuna <alexander.tuna@gmail.com>
2014-11-27 15:25:06 +0100

    playing with md

--- scripts/pages.sh
+++ scripts/pages.sh
@@ -1,2 +1,2 @@
-echo [$(date +%Y-%m-%d-%Hh%Mm%Ss)] $(pdfinfo thesis.pdf | grep Pages | tr -d "Pages: ") >> pages.md
+echo + [$(date +%Y-%m-%d-%Hh%Mm%Ss)] $(pdfinfo thesis.pdf | grep Pages | tr -d "Pages: ") >> pages.md
 
