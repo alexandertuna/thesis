8de8fe1fc816c01ec482e209b9fc7e7d05aa79b3
alexandertuna <alexander.tuna@gmail.com>
2014-12-09 23:29:30 +0100

    add MC SR plots

---
+++ scripts/scp_MC.sh
@@ -0,0 +1,17 @@
+# a script to retrieve plots from at3i00. run from the top dir.
+# [thesis] > . scripts/scp_LTT.sh
+
+# vbf
+input="atuna@at3i00.hep.upenn.edu:/web/Analysis/tuna/HiggsTauTau2012/scratch/higgs.plots.2014-12-09-16h52m17s/regions/VBFmvaSR/*/*"
+output="figures/analysis/vbf-MCXSR"
+
+mkdir -p ${output}
+
+for plot in "H-pt-hi" "lep-eta-centrality" "mMMC" "mT" "met-phi-centrality" "mvis" "n-jets30" "taulep-dR" "dijet-m-high" "jet-1-eta" "jet-1-pt" "jet-2-eta" "jet-2-pt" "jets-deta" "jets-dphi" "jets-etaprod" "system-pt" "lep-eta" "lep-pt-hi" "met-pt-hi" "BDTEve-VBF" "tau-eta" "tau-numTrack" "tau-pt"; do
+    for ext in "eps" "pdf"; do
+        scp ${input}/h1-${plot}.${ext} ${output}/${plot}.${ext}
+    done
+done
+
+
+
